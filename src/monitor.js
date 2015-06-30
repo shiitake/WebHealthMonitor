64ab0a2fcddaf0c8d963239f2314006cdb7d8257
Stephen England <Stephen.England@DealerTrack.com>
2015-06-30 15:10:49 -0500

    Split out monitor endpoint into it's own view/viewmodel

--- src/monitor.js
+++ src/monitor.js
@@ -1,6 +1,7 @@
 import {computedFrom} from 'aurelia-framework';
 import {inject} from 'aurelia-framework';
 import {HttpClient} from 'aurelia-http-client';
+import {MonitorEndpoint} from 'monitorEndpoint';
 
 @inject(HttpClient)
 export class Monitor {
@@ -68,82 +69,6 @@ export class EndpointGroup {
 
 }
 
-//monitor
-export class MonitorEndpoint {
-  status = "unknown";
-  intervalSpeed = 6000;
-  checking = false;
-  currentData = {};
-
-  constructor(options, http) {
-    this.http = http;
-    this.name = options.name;
-    this.url = options.url;
-    if (options.status) {
-      this.status = options.status;
-    }
-    this.startMonitor();
-  }
-
-  
-  startMonitor() {
-    var endpoint = this;
-    endpoint.checking = true;
-
-    endpoint.http.jsonp(endpoint.url, 'callback')
-      .then(response => {
-        endpoint.checking = false;
-        endpoint.status = "ok";
-        endpoint.currentData = response.response;
-        setTimeout(function () { endpoint.startMonitor(); }, endpoint.intervalSpeed);
-      })
-      .catch(response => {
-        this.checking = false;
-        this.status = "error";
-        setTimeout(function () { endpoint.startMonitor(); }, endpoint.intervalSpeed);
-      });
-  }
-
-  @computedFrom('status')
-  get statusCss() {
-    switch (this.status.toLowerCase()) {
-      case "ok": return "panel-success";
-      case "unknown": return "panel-default";
-      default: return "panel-danger";
-    }
-  }
-
-  @computedFrom('status')
-  get isDown() {
-    switch (this.status.toLowerCase()) {
-      case "ok": 
-      case "unknown": return false;
-      default: return true;
-    }
-  }
-
-  @computedFrom('currentData')
-  get dataDetails() {
-    var details = [];
-    for (var key in this.currentData) {
-      if (!this.isIgnoredDetail(key, this.currentData)) {
-        details.push({
-          "name": key,
-          "value": this.currentData[key]
-        }); 
-      }
-    }
-
-    return details;
-  }
-
-  isIgnoredDetail(detailKey, data) {
-    switch (detailKey) {
-      case "ui": return true;
-    }
-    return data.ui && data.ui.hide && data.ui.hide[detailKey];
-  }
-}
 
 //search
 export class SearchGroups {
